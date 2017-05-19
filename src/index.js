819b5b9779b745420055dda787b25ea40aefa9f0
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-19 12:56:35 +0200

    update rca and appify

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App'
 import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+
+registerServiceWorker()
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'))
